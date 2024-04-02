c6e12c3a60d59374d51c106cf3b085d688cefeb1
Maria <masya.2506@mail.ru>
2024-04-02 17:17:37 +0300

    project2

--- homework5/spiders/footballkulichkinet.py
+++ homework5/spiders/footballkulichkinet.py
@@ -14,12 +14,8 @@ class FootballkulichkinetSpider(scrapy.Spider):
             link = team.xpath(".//@href").get()
 
             yield response.follow(url=link, callback=self.parse_team, meta={'team_name' : name})
-
-
-            #yield {
-             #   'name' : name,
-              #  'link' : link}
     
+
     def parse_team(self, response):
         rows = response.xpath("//tr/td/font/b/a")
         for row in rows:
@@ -29,38 +25,3 @@ class FootballkulichkinetSpider(scrapy.Spider):
                 'team_name' : name, 
                 'coach' : coach
                 }
-
-
-        """_summary_
-
-        Yields:
-            _type_: _description_
-        
-
-        balls = response.xpath("//tr/td/font/b")
-
-        for ball in balls:
-            numbers = ball.xpath(".//text()").get().strip()
-            yield {
-            'team_name' : name, 
-            'balls' : numbers
-            }
-
-        
-
-        """
-        #yield response.follow(url=link, callback=self.parse_teams, meta={'team_name' : name})
-
-   # def
-
-"""
-    def parse_teams(self, response):
-        rows = response.xpath("//tr[contains(@class, 'col2 inl vtop')]")
-        for row in rows:
-            balls = row.xpath(".//td/font/b").get().strip()
-            name = response.request.meta['team_name']
-        
-        yield {
-            'team_name' : name, 
-            'balls' : balls
-            """ 
